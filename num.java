41469b306842f1865f65606526eb1df6ccd1f385
Prashant Savita <ps4878383@gmail.com>
2026-02-07 14:04:22 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,18 @@
+public class num {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double quotient = (double) num1 / num2;
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
